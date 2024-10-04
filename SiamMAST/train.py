25984f70e8c3c19f9b1fe239198dcd7ed903909a
anview <140043405+zengminxu@users.noreply.github.com>
2024-10-04 13:57:16 +0800

    Update train.py

--- SiamMAST/train.py
+++ SiamMAST/train.py
@@ -14,20 +14,20 @@ def train(model, train_loader, criterion, optimizer, num_epochs):
             loss.backward()
             optimizer.step()
 
-        # 计算并打印准确率
+
         accuracy = calculate_accuracy(outputs, labels)
         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}, Accuracy: {accuracy:.4f}')
 
 if __name__ == "__main__":
     model = SiamMAST()
 
-    # 选择损失函数和优化器
+
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
     train_dataset = VideoDataset()  
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
-    # 训练过程
+
     num_epochs = 10
     train(model, train_loader, criterion, optimizer, num_epochs)
