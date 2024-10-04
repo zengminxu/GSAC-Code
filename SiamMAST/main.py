eb2f43620b8a72bfbe06383c1f8a05f6132ea818
anview <140043405+zengminxu@users.noreply.github.com>
2024-10-04 22:23:08 +0800

    Update main.py

--- SiamMAST/main.py
+++ SiamMAST/main.py
@@ -4,15 +4,15 @@ from model.siam_mast import SiamMAST
 def main():
     model = SiamMAST()
 
-    # 损失函数和优化器
+
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    # 加载数据集
+
     train_dataset = VideoDataset()  
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
-    # 训练
+
     num_epochs = 10
     train(model, train_loader, criterion, optimizer, num_epochs)
 
