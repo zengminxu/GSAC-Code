1a681f929b9ff2aff4773f7dcdcadeb6b1ece5ba
anview <140043405+zengminxu@users.noreply.github.com>
2024-10-04 13:49:37 +0800

    Update train.py

--- train.py
+++ train.py
@@ -12,23 +12,34 @@ from model import GSSL
 import torch
 import torch.nn as nn
 
-args = parser.parse_args()  
+args = parser.parse_args() 
 
 data_path = './Data/{}'.format(args.dataset)  
 train_features, train_labels, train_adj, test_features, test_labels, test_adj = load_data(data_path, args.dataset,
                                                                                           args.k)
-train_edge = torch.nonzero(train_adj).T 
+# train_features_hmdb51:(1530,768)
+train_edge = torch.nonzero(train_adj).T  # Find out the positions of those elements that are not 0 in the adjacency matrix of the training data as edges
 test_edge = torch.nonzero(test_adj).T
 
 seed = 43
 random.seed(seed)
+# random.seed() 
+np.random.seed(seed)
 torch.manual_seed(seed)  
+# Set seeds for the CPU to generate random numbers so that the result is certain.
+# When you set a random seed, the following random algorithm generates numbers according to the current random seed according to a certain law.
+# Random seed scope is from setup time to next setup time. To replicate the results, just set the same random seeds.
 torch.cuda.manual_seed(seed)  
 
+# Model and optimizer
+
+# with open(r"a:\wf\Desktop\KNN_best\node_embeddings\node_embeddings.pkl", "rb") as file:
+#     node_embeddings = pickle.load(file)
+
+ # Gets the dimension of the node embedding vector
+# input_dim = node_embeddings.shape[1]
 
 
-in_dim = train_features.shape[1]
-print("in_dim的大小为:", in_dim)
 model = GSSL(in_dim=train_features.shape[1],
              hid_dim=args.hidden,
              hid0_dim=args.hidden0,
@@ -56,6 +67,8 @@ time_total = time.time()
 for epoch in range(args.epochs):
     loss = train(epoch, model, optimizer, train_features, train_labels, train_edge, args)
 
+#    if epoch % args.save_freq == 0:
+#        writer.add_scalar('Train_Loss', loss, epoch)
 
     scheduler.step()
 
