d942459330501b8aa1a34dfd5f0b9235b10c365e
anview <140043405+zengminxu@users.noreply.github.com>
2024-10-04 13:54:06 +0800

    Update dataset.py

--- SiamMAST/dataset.py
+++ SiamMAST/dataset.py
@@ -3,15 +3,14 @@ from torch.utils.data import Dataset
 
 class VideoDataset(Dataset):
     def __init__(self, transform=None):
-        # 初始化数据集，加载视频和标签
+    # Initialize data set, load videos and tags
         self.transform = transform
-        self.data = []  # 在此处加载数据，例如视频路径和标签
+        self.data = [] 
 
     def __len__(self):
-        return len(self.data)  # 返回数据集大小
+        return len(self.data)  
 
     def __getitem__(self, idx):
-        # 加载视频帧和标签
         video1 = self.data[idx]['video1']
         video2 = self.data[idx]['video2']
         label = self.data[idx]['label']
